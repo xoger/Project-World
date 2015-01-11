22e3a541e0d726567c82d6055d75adcd18e92778
xoger <matthewparr93@gmail.com>
2015-01-11 12:06:33 +0000

    Revert "Added trees"
    This reverts commit 60aa220fb5e9781e77bd2b53f8c572634401172e.
    
    Conflicts:
    	Project World/Project World.sdf
    	Project World/Project World.v12.suo
    	Project World/Project World/Debug/Project World.log

--- Project World/Project World/main.cpp
+++ Project World/Project World/main.cpp
@@ -60,7 +60,8 @@ public:
 
 		camera = scnmgr->addCameraSceneNodeFPS(NULL,100,1,-1,keyMap,8);
 		camera->setFarValue(100000);
-		camera->setPosition(vector3df(0, 1000, 0));
+		camera->setPosition(vector3df(0, 0, 0));
+		camera->setInputReceiverEnabled(false);
 		
 		maploader = new MapLoader();
 		objects = maploader->Start(scnmgr, driver);
@@ -86,7 +87,7 @@ public:
 		static int frame = 1;
 		if (frame == 200)
 		{
-			cout << driver->getFPS() << endl;
+			//cout << driver->getFPS() << endl;
 			frame = 0;
 		}
 		frame++;
